bc18eac953aed4709749096a09a41e9b2345550f
Shanezatch <sk0551460@gamil.com>
2023-04-10 13:46:58 +0530

    Project files

---
+++ Source/exception.py
@@ -0,0 +1,20 @@
+import sys,os
+import inspect
+
+
+def error_message_details(error, error_detail):
+    _, _, exc_tb = error_detail
+    filename = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = f"Error occurred in {filename}, line {line_number}: {error}"
+    return error_message                                                                 
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, exc_info):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message, exc_info)
+
+    def __str__(self):
+        return self.error_message
+
