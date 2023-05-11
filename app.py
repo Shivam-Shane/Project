82c41857b5fb55ad575152b6a2a8d20004a88dd2
Shanezatch <sk0551460@gamil.com>
2023-05-12 02:17:05 +0530

    modified the returned result value

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
 from flask import Flask,request,render_template
-import numpy as np
 import pandas as pd
-from datetime import datetime
 from Source.logger import logging
 
 
-logging.info("importing file")
-from Pipelines.Predict_Pipeline import PredictPipeline
-from Pipelines.Predict_Pipeline import CustomData
+from Pipelines.Predict_Pipeline import PredictPipelineClass
+from Pipelines.Predict_Pipeline import CustomDataClass
 
 
 logging.info("Starting application")
-application=Flask(__name__)
+app=Flask(__name__)
 
-app=application
 
-## route for home page
-logging.info("Starting index html")
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -25,9 +20,9 @@ def index():
 @app.route('/predict_data',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
-        return render_template('Home.html')
+        return render_template('home.html')
     else:
-        data=CustomData(
+        data=CustomDataClass(
                 Date_Received=pd.to_datetime(request.form.get('Date_Received')),
                 Product_Name=request.form.get('Product_Name'),
                 Issue_Detail=request.form.get('Issue_Detail'),
@@ -40,14 +35,13 @@ def predict_datapoint():
 
         Prediction_dataframe=data.get_data_as_dataframe()
 
-        Predict_pipeline_object=PredictPipeline()
+        Predict_pipeline_object=PredictPipelineClass()
 
         
         result=Predict_pipeline_object.predict_dataset(Prediction_dataframe)
         logging.info("rendering the result to webpage")
-        return render_template('home.html',result=result[0])
-logging.info("Its done")
+        return render_template('home.html',result=result)
 
 if __name__=="__main__":
-        app.run(host="0.0.0.0",debug=True)
+        app.run(host="0.0.0.0",debug=True,port=8080)
     
