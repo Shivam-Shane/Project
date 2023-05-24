88d34b68503b936410096821e4c3d372d7e227e0
Shanezatch <sk0551460@gamil.com>
2023-05-25 01:15:10 +0530

    added comments

--- Source/Model_function/Data_model.py
+++ Source/Model_function/Data_model.py
@@ -2,29 +2,26 @@ from Source.logger import logging
 from Source.exception import CustomExceptionClass
 from Source.utils import save_objects_file
 from dataclasses import dataclass
-import os,sys
-import pandas as pd
-import numpy as np
+import os
+import sys
 from sklearn.tree import DecisionTreeClassifier
-import warnings
-warnings.filterwarnings('ignore')
 
 @dataclass
 class DataModelConfig:
-    model_trained_file=os.path.join('Assets',"Model_Trained.pkl")
+    model_trained_file=os.path.join('Assets',"Model_Trained.pkl")# directory for saving model pickle file\
 
 class DataModelClass:
     try:
-        def initiate_data_model(self,train_array,train_traget_array,test_array,test_traget_array):
-            logging.info("Data Modeling initiated")
+        def initiate_data_model(self,data_array,data_traget_array):# data 
+            logging.info("Data modeling initiated")
 
             model_DTC = DecisionTreeClassifier()
-            model_DTC.fit(train_array, train_traget_array)## model fitting on training data
-            logging.info("Model {} succefully trainied".format(model_DTC))
+            model_DTC.fit(data_array, data_traget_array)## model fitting on data
+            logging.info("Model {} successfully trained".format(model_DTC))
             
             save_objects_file(
                         file_path=DataModelConfig.model_trained_file,
-                        object=model_DTC
+                        object=model_DTC #model object
                              )
     except Exception as e:
         logging.error(str(e))
